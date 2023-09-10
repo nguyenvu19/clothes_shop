eb72a403f6e1fac8c2d564961c5a9c77513fddcc
Nguyen Vu <nguyenvu191000@gmail.com>
2023-09-10 14:19:24 +0700

    security

--- src/main/java/nguyenvu/clothes_shop/filter/JwtFilter.java
+++ src/main/java/nguyenvu/clothes_shop/filter/JwtFilter.java
@@ -1,10 +1,7 @@
 package nguyenvu.clothes_shop.filter;
 
 import io.jsonwebtoken.Claims;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 import nguyenvu.clothes_shop.utils.JwtHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,6 +10,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
